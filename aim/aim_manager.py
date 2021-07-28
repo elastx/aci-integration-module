e2b7c377c0ccd6261b7a4278d82d52c5f6cf328b
Sayali Naval <sayalinaval@gmail.com>
2021-07-28 11:00:26 -0700

    AIM command changes for System SG
    Change-Id: I97f3c4ddbf429a4463b76482dbb9322076b78cb1

--- aim/aim_manager.py
+++ aim/aim_manager.py
@@ -89,6 +89,8 @@ class AimManager(object):
                      api_res.SecurityGroup,
                      api_res.SecurityGroupSubject,
                      api_res.SecurityGroupRule,
+                     api_res.SystemSecurityGroup,
+                     api_res.SystemSecurityGroupRule,
                      api_res.Configuration,
                      api_service_graph.DeviceCluster,
                      api_service_graph.DeviceClusterInterface,
--- aim/aim_store.py
+++ aim/aim_store.py
@@ -240,6 +240,8 @@ class SqlAlchemyStore(AimStore):
                     api_res.SecurityGroup: models.SecurityGroup,
                     api_res.SecurityGroupSubject: models.SecurityGroupSubject,
                     api_res.SecurityGroupRule: models.SecurityGroupRule,
+                    api_res.SystemSecurityGroup: models.SystemSecurityGroup,
+                    api_res.SystemSecurityGroupRule: models.SystemSecurityGroupRule,
                     api_res.Configuration: config_model.Configuration,
                     api_tree.Tree: tree_model.Tree,
                     api_tree.ConfigTree: tree_model.ConfigTree,
